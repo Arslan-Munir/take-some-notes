9aa50b7643cf2b2f8cf57ecbbb993104a3d8e7e7
Arslan Munir <Arslan Munir>
2020-12-23 18:11:47 +0500

    Formated home page

---
+++ src/app/modules/shared/models/error.model.ts
@@ -0,0 +1,15 @@
+export class Errors {
+
+  private errors = new Map([
+    ['auth/argument-error', 'Invalid credentials'],
+    ['auth/wrong-password', 'Invalid credentials'],
+    ['auth/user-not-found', 'Invalid credentials'],
+    ['auth/weak-password', 'Password should be at least 6 characters'],
+    ['auth/email-already-in-use', 'Email is already taken'],
+    ['auth/too-many-requests', 'Account is temporary disable due to too many login requests. Please try latter.'],
+  ]);
+
+  getErrorDetails(errorCode): string {
+    return this.errors.get(errorCode);
+  }
+}
